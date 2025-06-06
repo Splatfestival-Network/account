07979bf90ea7e716b733aebbf7d410ffddf29442
DJMrTV <tvnebel@gmail.com>
2025-06-06 11:59:45 +0200

    fix: allow basic auth on token generation

--- src/json_api/oauth/generate_token.rs
+++ src/json_api/oauth/generate_token.rs
@@ -5,7 +5,7 @@ use crate::nnid::oauth::generate_token::token_type::AUTH_TOKEN;
 use crate::Pool;
 
 #[get("/api/v2/oauth2/generate_token")]
-pub async fn generate_token(pool: &State<Pool>, auth: Auth<true>) -> String{
+pub async fn generate_token(pool: &State<Pool>, auth: Auth<false>) -> String{
     let pool = pool.inner();
 
 
